7abb3f0d9e2d963691f2cecb441da18b9d5ff3ad
micky-meecky <1459654102@qq.com>
2023-04-30 00:30:18 +0800

    add dataset

--- train.py
+++ train.py
@@ -162,7 +162,7 @@ def breast_loader():
     fold_k = 5
     fold_idx = 1
     fold_id = 1
-    batch_size = 30     # -------------------------------------------------------
+    batch_size = 20     # -------------------------------------------------------
     distance_type = "dist_mask"
     normal_flag = False
     image_size = 256
