ff9ccb77fcb4a7eee8ee456717fcec36d7630544
micky-meecky <1459654102@qq.com>
2023-05-02 22:31:00 +0800

    try cifar10
    bs = 30,Unet_3,e = 1300,lr = 0.005,numworker = 5

--- mymodels/OpenDataSet.py
+++ mymodels/OpenDataSet.py
@@ -3,17 +3,22 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+import os
 
 
 def Cifar_10(bs):
+
+    # 打印当前目录
+    print(os.getcwd())
+
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = torchvision.datasets.CIFAR10(root='../data', train=True,
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs,
                                               shuffle=True, num_workers=2)
-    testset = torchvision.datasets.CIFAR10(root='../data', train=False,
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=bs,
                                              shuffle=False, num_workers=2)
