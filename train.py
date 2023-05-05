2b2fe372d10178f096339015ac865520c0af4b3a
micky-meecky <1459654102@qq.com>
2023-05-05 23:23:02 +0800

    change to BCE + sigmoid due to binary classification add seg part. and add sigmoid activation. bs = 20,unetr_cls2seg_bce_1,e = 700,lr = 1e-5,numworker = 6

--- train.py
+++ train.py
@@ -538,7 +538,7 @@ def Train_Mnist():
 if __name__ == '__main__':
     # Train_Mnist()
     project = 'unetr_cls2seg_bce_1'
-    bs = 20
+    bs = 30
     model_name = 'unetr'
     use_pretrained = False
 
