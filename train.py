6fcedfdc67976e93bee7eda24d7a29f7bbc7da74
micky-meecky <1459654102@qq.com>
2023-05-09 14:26:38 +0800

    change FC back to the best bs = 30,unetr_cls2seg_olseg_bce_1,e = 500,lr = 1e-4/5,numworker = 6, 224

--- train.py
+++ train.py
@@ -546,7 +546,7 @@ def Train_Mnist():
 if __name__ == '__main__':
 
     Train_breast('unetRcls_ocls2_3', 30, 300, 'unetr', 1e-4, True, False, False)
-    Train_breast('unetRseg_olseg_0', 30, 550, 'unetr', 1e-4, False, True, True)
+    Train_breast('unetRseg_olseg_0', 30, 300, 'unetr', 1e-4, False, True, True)
     # Train_breast('UNet_olseg_0', 10, 550, 'unet', 1e-4, False, True, True)
     # Train_breast('efficientnetb7_cls2_0', 30, 'efficientnet', 1e-4, True, False)
     # Train_breast('resnet101_cls2bce_1', 20, 'resnet101', 1e-5, True, False)
