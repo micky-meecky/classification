a4c97aa33da8fea837fb08ac2a2217f274532666
micky-meecky <1459654102@qq.com>
2023-06-17 00:32:34 +0800

    try only cls task. tiaocan UnetR_ocls_5B, e = 330,lr = 4e-6, numworker = 0, 224, bs = 32, channel = 1,

--- train.py
+++ train.py
@@ -567,7 +567,7 @@ if __name__ == '__main__':
     # Train_breast('UNet_olseg_0', 10, 600, 'unet', 1e-2, False, True, True, False)
     # Train_breast('unetRseg_cls_seg_8', 5, 100, 'unetr', 9.63366620781354e-14, False, True, _only_segtask=False,
     #              is_continue_train=True)
-    Train_breast('UnetR_ocls_5B', 32, 400, 'unetr', 4e-6,
+    Train_breast('UnetR_ocls_5B', 32, 330, 'unetr', 4e-6,
                  Use_pretrained=False,
                  _have_segtask=False,
                  _only_segtask=False,
