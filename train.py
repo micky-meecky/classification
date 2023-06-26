d9e42716aac4469e93c3bc05234aac5dee38d5a5
“micky-meecky” <1459654102@qq.com>
2023-06-27 00:28:34 +0800

    unetr 8 layernum,on server UnetR_cls_seg_62_63, e = 400,lr = 4/2e-4, numworker = 0, 224, bs = 16,channel = 1,

--- train.py
+++ train.py
@@ -713,16 +713,16 @@ if __name__ == '__main__':
     testf1.append(test_f1_score)
     testacc.append(test_acc)
 
-    test_precision, test_recall, test_f1_score, test_acc = \
-        Train_breast('UnetR_cls_seg_63', 16, 800, 'unetr', 2e-4,
-                     Use_pretrained=False,
-                     _have_segtask=True,
-                     _only_segtask=False,
-                     is_continue_train=False)
-    testp.append(test_precision)
-    testr.append(test_recall)
-    testf1.append(test_f1_score)
-    testacc.append(test_acc)
+    # test_precision, test_recall, test_f1_score, test_acc = \
+    #     Train_breast('UnetR_cls_seg_63', 16, 800, 'unetr', 2e-4,
+    #                  Use_pretrained=False,
+    #                  _have_segtask=True,
+    #                  _only_segtask=False,
+    #                  is_continue_train=False)
+    # testp.append(test_precision)
+    # testr.append(test_recall)
+    # testf1.append(test_f1_score)
+    # testacc.append(test_acc)
 
     # test_precision, test_recall, test_f1_score, test_acc = \
     #     Train_breast('UnetR_cls_seg_64', 16, 400, 'unetr', 1e-4,
