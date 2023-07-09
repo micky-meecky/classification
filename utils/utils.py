2e97177f94a863670c5ba00555e3642f635916bf
“micky-meecky” <1459654102@qq.com>
2023-07-09 17:02:42 +0800

    not use clip. Unet_ocls_1/2, e = 400,lr = 6e-4, numworker = 0, 224, bs = 128,channel = 1,

--- utils/utils.py
+++ utils/utils.py
@@ -429,15 +429,15 @@ if __name__ == '__main__':
     lr = 0.00024681865315859415  # 初始学习率
     lr_low = 1e-13  # 最低学习率
     decay_step = 10  # 每decay_step个epoch衰减一次
-    decay_ratio = 0.935  # 每decay_step个epoch衰减一次，衰减比例为decay_ratio
+    decay_ratio = 0.925  # 每decay_step个epoch衰减一次，衰减比例为decay_ratio
     num_epochs_decay = 100   # 从第几个epoch开始衰减
-    lr_warm_epoch = 1   # warmup的epoch数
-    lr_cos_epoch = 800  # cos衰减的epoch数
+    lr_warm_epoch = 10   # warmup的epoch数
+    lr_cos_epoch = 250  # cos衰减的epoch数
     optimizer = torch.optim.Adam([torch.randn(3, 3)], lr=lr)
     lr_sch = LrDecay(lr_warm_epoch, lr_cos_epoch, lr, lr_low, optimizer)
     # 初始化一个学习率列表
     lr_list = []
-    for epoch in range(940):
+    for epoch in range(400):
         lr_sch, optimizer = AdjustLr(lr_sch, optimizer, epoch, lr_cos_epoch, lr_warm_epoch, num_epochs_decay,
                                      GetCurrentLr(optimizer), lr_low, decay_step, decay_ratio)
         # 将学习率记录在一个列表lr_list中
