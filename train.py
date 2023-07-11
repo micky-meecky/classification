6d5feb25899d72d3c5bbb3016f7ac5cf5a644e6e
“micky-meecky” <1459654102@qq.com>
2023-07-11 23:14:07 +0800

    not use clip. Unet_cls_40, e = 600,lr = dynamic, numworker = 0, 224, bs = 16,channel = 1, aug = 0.0 not use clip. Unet_cls_50, e = 600,lr = dynamic, numworker = 0, 224, bs = 8,channel = 1, aug = 0.5

--- train.py
+++ train.py
@@ -710,7 +710,7 @@ if __name__ == '__main__':
     test_precision, test_recall, test_f1_score, test_acc = \
         Train_breast('Unet_cls_seg_40', 16, 600, 'unet', 4e-4,
                      Use_pretrained=False,
-                     _have_segtask=False,
+                     _have_segtask=True,
                      _only_segtask=False,
                      is_continue_train=False,
                      use_clip=False)
@@ -729,7 +729,7 @@ if __name__ == '__main__':
     test_precision, test_recall, test_f1_score, test_acc = \
         Train_breast('Unet_cls_seg_50', 8, 600, 'unet', 4e-4,
                      Use_pretrained=False,
-                     _have_segtask=False,
+                     _have_segtask=True,
                      _only_segtask=False,
                      is_continue_train=False,
                      use_clip=False)
