c88b20a0ff6c9a7a3babe8f12f2e1d14b3e9afe1
“micky-meecky” <1459654102@qq.com>
2023-07-12 23:56:44 +0800

    not use clip. Unet_cls_seg_30, e = 600,lr = dynamic, numworker = 0, 224, bs = 16,channel = 1, aug = 0.8

--- train.py
+++ train.py
@@ -685,7 +685,7 @@ if __name__ == '__main__':
     testacc = []
 
     test_precision, test_recall, test_f1_score, test_acc = \
-        Train_breast('Unet_cls_seg_20', 8, 600, 'unet', 4e-4,
+        Train_breast('Unet_cls_seg_30', 16, 600, 'unet', 4e-4,
                      Use_pretrained=False,
                      _have_segtask=True,
                      _only_segtask=False,
