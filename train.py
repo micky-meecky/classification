0b5328427a27dd68e4fa608beadb2217fe91e1aa
“micky-meecky” <1459654102@qq.com>
2023-07-28 13:45:39 +0800

    Unet_oseg_ch3_256_00,preres101AGUnet_cls_seg_ch3_512_00, e = 300/800,lr = 6e-4, numworker = 0, 256, bs = 20/6,channel = 3, aug = 0.5

--- train.py
+++ train.py
@@ -725,7 +725,7 @@ if __name__ == '__main__':
     test_precision, test_recall, test_f1_score, test_acc = \
         Train_breast('preres101AGUnet_cls_seg_ch3_512_00', 6, 800, 'res101UNetsmp', 6e-4,
                      Use_pretrained=True,
-                     _have_segtask=False,
+                     _have_segtask=True,
                      _only_segtask=False,
                      is_continue_train=False,
                      use_clip=False,
