efd187aaec12d1118e1e2eac44ad1959560ef655
“micky-meecky” <1459654102@qq.com>
2023-08-09 21:23:31 +0800

    swinViTseg_seg_cls_ch3_224_00，e = 1000,lr = 1e-3, numworker = 0, 512，bs = 5,channel = 3, aug = 0.5

--- train.py
+++ train.py
@@ -722,27 +722,27 @@ if __name__ == '__main__':
         print(testf1[i], end=', ')
         print(testacc[i])
 
-    test_precision, test_recall, test_f1_score, test_acc = \
-        Train_breast('ViT_ocls_ch3_224_00', 6, 1000, 'ViT', 1e-3,
-                     Use_pretrained=False,
-                     _have_segtask=False,
-                     _only_segtask=False,
-                     is_continue_train=False,
-                     use_clip=False,
-                     channel=3,
-                     size=224,
-                     decayepoch=990)
-    testp.append(test_precision)
-    testr.append(test_recall)
-    testf1.append(test_f1_score)
-    testacc.append(test_acc)
-
-    for i in range(len(testp)):
-        print('第' + str(i + 1) + '个实验结果：', end=', ')
-        print(testp[i], end=', ')
-        print(testr[i], end=', ')
-        print(testf1[i], end=', ')
-        print(testacc[i])
+    # test_precision, test_recall, test_f1_score, test_acc = \
+    #     Train_breast('ViT_ocls_ch3_224_00', 6, 1000, 'ViT', 1e-3,
+    #                  Use_pretrained=False,
+    #                  _have_segtask=False,
+    #                  _only_segtask=False,
+    #                  is_continue_train=False,
+    #                  use_clip=False,
+    #                  channel=3,
+    #                  size=224,
+    #                  decayepoch=990)
+    # testp.append(test_precision)
+    # testr.append(test_recall)
+    # testf1.append(test_f1_score)
+    # testacc.append(test_acc)
+    #
+    # for i in range(len(testp)):
+    #     print('第' + str(i + 1) + '个实验结果：', end=', ')
+    #     print(testp[i], end=', ')
+    #     print(testr[i], end=', ')
+    #     print(testf1[i], end=', ')
+    #     print(testacc[i])
 
 
 
