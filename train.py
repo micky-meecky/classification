b9c3461b300ed1d7ce9e15099a96dc2b0c294606
“micky-meecky” <1459654102@qq.com>
2023-08-16 17:03:06 +0800

    add Transunet. ViTcls_ocls_BUSI_ch3_224_00, Transunet_ocls_BUSI_ch3_224_00, e = 1200,lr = 6e-4, numworker = 0, 224，bs = 6,channel = 1/3, aug = 0.5

--- train.py
+++ train.py
@@ -734,21 +734,21 @@ if __name__ == '__main__':
     #     print(testf1[i], end=', ')
     #     print(testacc[i])
 
-    # test_precision, test_recall, test_f1_score, test_acc = \
-    #     Train_breast('ViTcls_ocls_BUSI_ch3_224_00', 6, 300, 'ViTcls', 6e-4,
-    #                  Use_pretrained=False,
-    #                  _have_segtask=False,
-    #                  _only_segtask=False,
-    #                  is_continue_train=False,
-    #                  use_clip=False,
-    #                  channel=3,
-    #                  size=224,
-    #                  decayepoch=290,
-    #                  datasc='BUSI')
-    # testp.append(test_precision)
-    # testr.append(test_recall)
-    # testf1.append(test_f1_score)
-    # testacc.append(test_acc)
+    test_precision, test_recall, test_f1_score, test_acc = \
+        Train_breast('ViTcls_ocls_BUSI_ch3_224_00', 6, 300, 'ViTcls', 6e-4,
+                     Use_pretrained=False,
+                     _have_segtask=False,
+                     _only_segtask=False,
+                     is_continue_train=False,
+                     use_clip=False,
+                     channel=3,
+                     size=224,
+                     decayepoch=290,
+                     datasc='BUSI')
+    testp.append(test_precision)
+    testr.append(test_recall)
+    testf1.append(test_f1_score)
+    testacc.append(test_acc)
 
     for i in range(len(testp)):
         print('第' + str(i + 1) + '个实验结果：', end=', ')
