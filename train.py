0d4710a3211562c71c8de887f2e214371952530b
“micky-meecky” <1459654102@qq.com>
2023-08-21 16:29:19 +0800

    AgUnet_cls_seg_ch3_256_00, AgconvUnet_cls_seg_ch3_256_00, e = 800,lr = 6e-4, numworker = 0, 512，bs = 6,channel = 3, aug = 0.5

--- train.py
+++ train.py
@@ -757,14 +757,37 @@ if __name__ == '__main__':
     #     print(testacc[i])
 
     test_precision, test_recall, test_f1_score, test_acc = \
-        Train_breast('agconvUnet_cls_seg_ch3_512_00', 6, 800, 'agconvunet', 6e-4,
+        Train_breast('AgUnet_cls_seg_ch3_256_00', 6, 800, 'agunet', 6e-4,
                      Use_pretrained=False,
                      _have_segtask=True,
                      _only_segtask=False,
                      is_continue_train=False,
                      use_clip=False,
                      channel=3,
-                     size=512,
+                     size=256,
+                     decayepoch=790,
+                     datasc='BUSI')
+    testp.append(test_precision)
+    testr.append(test_recall)
+    testf1.append(test_f1_score)
+    testacc.append(test_acc)
+
+    for i in range(len(testp)):
+        print('第' + str(i + 1) + '个实验结果：', end=', ')
+        print(testp[i], end=', ')
+        print(testr[i], end=', ')
+        print(testf1[i], end=', ')
+        print(testacc[i])
+
+    test_precision, test_recall, test_f1_score, test_acc = \
+        Train_breast('AgconvUnet_cls_seg_ch3_256_00', 6, 800, 'agconvunet', 6e-4,
+                     Use_pretrained=False,
+                     _have_segtask=True,
+                     _only_segtask=False,
+                     is_continue_train=False,
+                     use_clip=False,
+                     channel=3,
+                     size=256,
                      decayepoch=790,
                      datasc='BUSI')
     testp.append(test_precision)
