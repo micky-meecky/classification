864c78ff479023ca33f95203d0204f840be84176
“micky-meecky” <1459654102@qq.com>
2023-08-21 15:50:48 +0800

    the latest version is  512^2,convUnet_cls_seg_ch3_256_00, e = 800,lr = 6e-4, numworker = 0, 512，bs = 6,channel = 3, aug = 0.5

--- train.py
+++ train.py
@@ -757,7 +757,7 @@ if __name__ == '__main__':
     #     print(testacc[i])
 
     test_precision, test_recall, test_f1_score, test_acc = \
-        Train_breast('agUnet_cls_seg_ch3_512_00', 6, 800, 'convunet', 6e-4,
+        Train_breast('agUnet_cls_seg_ch3_512_00', 6, 800, 'agunet', 6e-4,
                      Use_pretrained=False,
                      _have_segtask=True,
                      _only_segtask=False,
