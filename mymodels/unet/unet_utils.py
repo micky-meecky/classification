cb6d0abc07a90476bf44926199da4840c3d75b9f
“micky-meecky” <1459654102@qq.com>
2023-08-24 23:14:11 +0800

    CasDilatedUNet_cls_seg_ch3_256_00, e = 800,lr = 6e-4, numworker = 0, 256，bs = 6,channel = 3, aug = 0.5

--- mymodels/unet/unet_utils.py
+++ mymodels/unet/unet_utils.py
@@ -50,7 +50,11 @@ class MultiDilatedConv(nn.Module):
     def __init__(self, in_channels, out_channels, dilation_rates=[1, 2]):
         super().__init__()
         self.convs = nn.ModuleList([
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=d, dilation=d, bias=False)
+            nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=d, dilation=d, bias=False),
+                nn.BatchNorm2d(out_channels),  # 添加批量归一化层
+                nn.ReLU(inplace=True)         # 添加ReLU激活函数
+            )
             for d in dilation_rates
         ])
         self.combine_conv = nn.Conv2d(out_channels * len(dilation_rates), out_channels, kernel_size=1)
