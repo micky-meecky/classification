14d897ea9a8dde6d31e36374e7eb0edcae8705ca
“micky-meecky” <1459654102@qq.com>
2023-08-26 15:02:53 +0800

    SideCBAMUNet_cls_seg_ch3_256_00, e = 800,lr = 6e-4, numworker = 0, 256，bs = 6,channel = 3, aug = 0.5

--- mymodels/CBAMUnet.py
+++ mymodels/CBAMUnet.py
@@ -442,11 +442,11 @@ class SideCBAMUNet(nn.Module):
         self.inc = (DoubleConv(n_channels, 64))
         self.inca = ChannelAttention(64)
         self.insa = SpatialAttention()
-        self.down1 = (SideDownwithCBAM(64, 128, sidemode='SE', method=Method, layernum=1))
-        self.down2 = (SideDownwithCBAM(128, 256, sidemode='SE', method=Method))
-        self.down3 = (SideDownwithCBAM(256, 512, sidemode='SE', method=Method))
+        self.down1 = (SideDownwithCBAM(64, 128, sidemode='SE3', method=Method, layernum=1))
+        self.down2 = (SideDownwithCBAM(128, 256, sidemode='SE3', method=Method))
+        self.down3 = (SideDownwithCBAM(256, 512, sidemode='SE3', method=Method))
         factor = 2 if bilinear else 1
-        self.down4 = (SideDownwithCBAM(512, 1024 // factor, sidemode='SE', method=Method, Islastlayer=True))
+        self.down4 = (SideDownwithCBAM(512, 1024 // factor, sidemode='SE3', method=Method, Islastlayer=True))
 
         # self.upsample = nn.Upsample(size=(256, 256), mode='bilinear', align_corners=True)
         self.up1 = (Up(1024, 512 // factor, bilinear))
