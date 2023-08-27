a3f14fa4349e5e914cd0407a0ab81e37405180fa
“micky-meecky” <1459654102@qq.com>
2023-08-27 22:52:27 +0800

    add M-UNet,M_UNet_oseg_ch1_256_00,M_UNet_oseg_ch1_256_01, e = 400,lr = 8e-3, numworker = 0, 256，bs = 6,channel = 3, aug = 0.5

--- train.py
+++ train.py
@@ -730,6 +730,30 @@ if __name__ == '__main__':
         print(testf1[i], end=', ')
         print(testacc[i])
 
+    test_precision, test_recall, test_f1_score, test_acc = \
+        Train_breast('M_UNet_oseg_ch1_256_01', 10, 400, 'M_UNet_seg', 1e-3,
+                     Use_pretrained=False,
+                     _have_segtask=True,
+                     _only_segtask=True,
+                     is_continue_train=False,
+                     use_clip=False,
+                     channel=1,
+                     size=256,
+                     decayepoch=390,
+                     datasc='BUSI',
+                     clsaux=False)
+    testp.append(test_precision)
+    testr.append(test_recall)
+    testf1.append(test_f1_score)
+    testacc.append(test_acc)
+
+    for i in range(len(testp)):
+        print('第' + str(i + 1) + '个实验结果：', end=', ')
+        print(testp[i], end=', ')
+        print(testr[i], end=', ')
+        print(testf1[i], end=', ')
+        print(testacc[i])
+
     # test_precision, test_recall, test_f1_score, test_acc = \
     #     Train_breast('CBAMUNet_cls_seg_ch3_256_00', 6, 800, 'CBAMUNet', 6e-4,
     #                  Use_pretrained=False,
