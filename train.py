30a3bdb61f20a00fef5b265e3365753f1f833dba
“micky-meecky” <1459654102@qq.com>
2023-09-03 22:24:57 +0800

    SideSE2AgCBAMUNet_cls_seg_ch3_256_06 e = 800,lr = 2e-3, numworker = 0, 256，bs = 16,channel = 3, aug = 0.5

--- train.py
+++ train.py
@@ -631,7 +631,7 @@ if __name__ == '__main__':
     testacc = []
 
     test_precision, test_recall, test_f1_score, test_acc = \
-        Train_breast('SideSE2AgCBAMUNet_cls_seg_ch3_256_05', 8, 800, 'SideAgCBAMUNet', 2e-3,
+        Train_breast('SideSE2AgCBAMUNet_cls_seg_ch3_256_06', 16, 800, 'SideAgCBAMUNet', 1e-3,
                      Use_pretrained=False,
                      _have_segtask=True,
                      _only_segtask=False,
