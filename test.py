ccafb36155a5a6e2c13a01fbe92cc5629c2cbd82
“micky-meecky” <1459654102@qq.com>
2023-11-05 16:20:20 +0800

    M_UNet_cls_seg_ch3_256_00_test,Adam e = 500,lr = 4e-4, numworker = 0, 256，bs = 10,channel = 1, aug = 0.5

--- test.py
+++ test.py
@@ -359,7 +359,7 @@ def test(mode: str, dataloader: DataLoader, model, SegImgSavePath, device: torch
                 sum(DClist) / len(DClist), sum(IOUlist) / len(IOUlist), sum(Acclist) / len(Acclist)))
 
     # 将结果保存到CSV文件
-    df = pd.DataFrame(sample_info, columns=['TrueLabel', 'PredictedProbability'])
-    df.to_csv('./model_predictions.csv', index=False)
+    # df = pd.DataFrame(sample_info, columns=['TrueLabel', 'PredictedProbability'])
+    # df.to_csv('./model_predictions.csv', index=False)
 
     return precision, recall, f1_score, acc
