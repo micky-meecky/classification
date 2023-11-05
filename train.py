e13bc87078be9a2cb2bd7971aa67be807a6d9924
“micky-meecky” <1459654102@qq.com>
2023-11-05 11:33:13 +0800

    SideSE2AgCBAMUNet_cls_seg_ch3_256_24_test,Adam e = 500,lr = 4e-4, numworker = 0, 256，bs = 10,channel = 1, aug = 0.5

--- train.py
+++ train.py
@@ -258,7 +258,7 @@ def Train_breast(Project, Bs, epoch, Model_name, lr, Use_pretrained, _have_segta
     contentvalid = "----per epoch training&vlidation test Time: "
     contentwholeepoch = "----whole epoch Time: "
     contenttotal = "----total cost: "
-    is_train = False
+    is_train = True
     is_test = True  # False
     last_epoch = last_epoch
     best_valid_acc = 0
